b93ecc7d1990269e9120f64bc7519165e38db2a7
Matija Strlekar <mstrlekar@gmail.com>
2022-06-16 14:38:12 +0200

    Init V1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # javascript-jquery-helper-functions
-Some useful functions
+Some useful functions in javascript or jQuery 
+
+#### Add Class on scroll (Jquery)
+
+Function that adds a class when arriving to certain element class
---
+++ jquery/addClassOnScroll.js
@@ -0,0 +1,29 @@
+ jQuery(document).ready(function($) {
+
+  function addClassOnScoll(class_name, anim_name) {
+
+      let cta_section = $(class_name);
+      let offset = cta_section.offset();
+      let w = $(window).height();
+      let section_position = offset.top - w;
+      let anim_class_name = anim_name;
+
+      $(window).scroll(function() {
+
+          let scroll = $(window).scrollTop();
+          if (scroll >= section_position) {
+              cta_section.addClass(anim_class_name);
+              console.log(section_position);
+
+          } else {
+              cta_section.removeClass(anim_class_name);
+          }
+      });
+
+  }
+
+  /* Example - on element cta-section we add class 'section-animation' for fire up*/
+  
+  addClassOnScoll('.cta-section', 'section-animation');
+
+ });
